de55d61ea9f799dac15451a9d5c1ce13fdfdfd71
Stephen Russell <srussell@alphaworks.tech>
2021-12-07 15:10:03 -0800

    Initial commit

---
+++ my_display_name_script.js
@@ -0,0 +1,32 @@
+jQuery(document).ready( function() {
+
+    jQuery("#linkbutton").click( function(e) {
+       e.preventDefault();
+       nonce = jQuery(this).attr("data-nonce");
+       post_title = jQuery(this).attr('post-title');
+       user = jQuery(this).attr('user')
+
+       jQuery.ajax({
+          type : "post",
+          dataType : "json",
+          url : myAjax.ajaxurl,
+          data : { action: "show_authors", 
+                  nonce: nonce, 
+                  post_title: post_title, 
+                  user: user },
+          success: function( response ) {
+             console.log( response );
+             const list = document.getElementById('list')
+             const item = document.createElement('li')
+             item.innerText = ` You have clicked ${response['click_count']} times.
+             This click was at ${response['date']}.
+             You are currently on the ${response['page_title']} page. `
+            list.appendChild(item)
+            //  jQuery('#linkbutton').hide();
+          }
+ 
+    })
+ 
+ })
+
+})
---
+++ shortcode_show_authors.php
@@ -0,0 +1,105 @@
+<?php 
+/**
+ * Plugin Name: Shortcode Submitted User Data
+ * Description: Show Authors using AJAX
+ * Version: 0.1
+ * Author: Stephen Russell
+ */
+/*
+    Challenge # 2 in ajax:
+
+    On button click, submit to user_meta_data, that the button was clicked, the time it was clicked and the page it was clicked on!
+
+    Return in the results a list of every time that user has clicked that button.
+
+        
+*/
+
+function show_authors() {
+
+    if(!wp_verify_nonce($_REQUEST['nonce'], "show_authors_nonce")) {
+        exit( "No naughty business please :)");
+    }
+
+    $user_meta = get_user_meta($_REQUEST['user'], "clicks");
+    $submitted = ($user_meta == '') ? 0 : count($user_meta);
+    date_default_timezone_set("America/Los_Angeles");
+
+    if ( $submitted == 0 ) {
+        $user_meta_array = array (
+            'click_count' => 1,
+            'date' => date("h:i:sa"),
+            'page_title' => $_REQUEST['post_title']
+        );
+        update_user_meta($_REQUEST['user'], "clicks", $user_meta_array);
+        $result = $user_meta_array;
+
+    } elseif ( $submitted > 0 ) {
+        $click_number = $submitted + 1;
+        $user_meta = array (
+            'click_count' => $click_number,
+            'date' => date("h:i:sa"),
+            'page_title' => $_REQUEST['post_title']
+        );
+        add_user_meta($_REQUEST['user'], "clicks", $user_meta);
+        $result = $user_meta;
+    }
+
+
+    
+    
+    if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') { // what exactly happens when this the button is pressed
+        $result = json_encode($result);
+        echo $result;
+     }
+     else {
+        header("Location: ".$_SERVER["HTTP_REFERER"]);
+     }
+  
+     die();
+}
+
+add_action('wp_ajax_show_authors', 'show_authors');
+
+function my_must_login() {
+    echo "You must log in to vote";
+    die();
+ }
+
+ add_action("wp_ajax_nopriv_show_authors", "my_must_login");
+
+
+function my_script_enqueuer() {
+    wp_register_script( "my_display_name_script", WP_PLUGIN_URL.'/show_author/my_display_name_script.js', array('jquery') );
+    wp_localize_script( 'my_display_name_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
+ 
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'my_display_name_script' );
+ 
+ }
+ 
+ add_action( 'init', 'my_script_enqueuer' );
+
+
+function display() {
+    $nonce = wp_create_nonce("show_authors_nonce");
+    ?>
+        <button 
+            id="linkbutton"
+            user="<?php echo get_current_user_id() ?>"
+            post-title="<?php echo get_the_title() ?>"
+            data-nonce="<?php echo $nonce; ?>">Click here to submit new information
+        </button>
+
+        <div>
+            <ul id="list">
+                
+            </ul>
+
+        </div>
+    <?php
+}
+
+ add_shortcode('shortcode_show_authors', 'display');
+
+
